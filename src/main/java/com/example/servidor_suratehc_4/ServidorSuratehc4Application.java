f7e143901e239bc31586445d24c44e036923387b
ssantiagoparra <sparracerveleon@gmail.com>
2024-09-17 13:53:03 -0500

    se crea proyecto

---
+++ src/main/java/com/example/servidor_suratehc_4/ServidorSuratehc4Application.java
@@ -0,0 +1,13 @@
+package com.example.servidor_suratehc_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorSuratehc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorSuratehc4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servidor_suratehc_4/ServidorSuratehc4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidor_suratehc_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorSuratehc4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
