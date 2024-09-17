ebbbebd27bdc9f6d7e45c81d46ff4bfa52db806f
ssantiagoparra <sparracerveleon@gmail.com>
2024-09-17 15:04:14 -0500

    actualizacion

---
+++ src/main/java/com/example/servidor_suratehc_4/MODELOS/Paciente.java
@@ -0,0 +1,19 @@
+package com.example.servidor_suratehc_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+    private Integer id;
+    private String nombres;
+    private Integer edad;
+    private LocalDate fechaNacimiento;
+    private String documento;
+    private String telefono;
+    private Integer copago;
+    private String email;
+    private String direccion;
+    private String ips;
+    private String medicoFamilia;
+
+}
